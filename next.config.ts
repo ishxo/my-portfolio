d6d3beb6fcf482774bcc5aeb9082443648598ee0
Ishxo <ishkhankostanyan@gmail.com>
2026-01-05 12:21:09 +0400

    fix/images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import nextI18NextConfig from "./next-i18next.config";
 
 const nextConfig: NextConfig = {
+  basePath: "/my-portfolio",
   images: {
     unoptimized: true,
   },
