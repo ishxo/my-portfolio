22cabd5a37bd1a0b73a9ab71c94fb4a2088d190c
Ishxo <ishkhankostanyan@gmail.com>
2026-01-05 12:48:19 +0400

    fix/images

--- src/app/components/Layout/Header/Logo/index.tsx
+++ src/app/components/Layout/Header/Logo/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Logo = () => {
   return (
     <Link href="/">
-      <Image src="/images/logo/logo.png" alt="logo" width={50} height={50} />
+      <Image src="images/logo/logo.png" alt="logo" width={50} height={50} />
     </Link>
   );
 };
