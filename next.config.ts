b01aec38f99107918301e7793873238f4780c493
Ishxo <ishkhankostanyan@gmail.com>
2026-01-05 12:34:04 +0400

    fix/images

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,33 @@
+// import type { NextConfig } from "next";
+// import nextI18NextConfig from "./next-i18next.config";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     unoptimized: true,
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   i18n: nextI18NextConfig.i18n,
+//   assetPrefix: process.env.NODE_ENV === "production" ? "/my-portfolio/" : "",
+//   basePath: process.env.NODE_ENV === "production" ? "/my-portfolio" : "",
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 import nextI18NextConfig from "./next-i18next.config";
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true, // Important for GitHub Pages
+    unoptimized: true, // disables next/image optimization
+    loader: "akamai", // prevent default _next/image behavior
+    path: "", // use relative paths
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
   i18n: nextI18NextConfig.i18n,
-  assetPrefix: process.env.NODE_ENV === "production" ? "/my-portfolio/" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/my-portfolio" : "",
   basePath: process.env.NODE_ENV === "production" ? "/my-portfolio" : "",
 };
 
